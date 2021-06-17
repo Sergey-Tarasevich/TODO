cc0050ca657adde2f651834647ad9170f2d0c6a1
Sergey Tarasevich <70640645+Sergey-Tarasevich@users.noreply.github.com>
2021-06-17 12:27:59 +0300

    feat: add Resux-Reducers

---
+++ src/Redux/Reducers/TodoReducer.js
@@ -0,0 +1,58 @@
+/*eslint default-case: "error"*/
+import {
+  ADD_TODO,
+  CLEAR_ALL_TODOS,
+  DELETE_TODO,
+  UPDATE_TODO,
+  TOGGLE_TODO,
+} from '../Actions/actionsTypes';
+
+import { todos } from '../states';
+
+export let reducer = (state = todos, action) => {
+  let todoTask;
+  switch (action.type) {
+    case ADD_TODO:
+      todoTask = [...state];
+      todoTask.push(action.payload);
+      return todoTask;
+
+    case CLEAR_ALL_TODOS:
+      todoTask = [...state];
+      todoTask = todoTask.filter((todo) => todo.id === action.payload);
+      return todoTask;
+
+    case DELETE_TODO:
+      todoTask = [...state];
+      todoTask = todoTask.filter((todo) => todo.id !== action.payload);
+      return todoTask;
+
+    case UPDATE_TODO:
+      todoTask = [...state];
+      let index = -1;
+      for (let i = 0; i < todoTask.length; i++) {
+        index++;
+        if (todoTask[i].id === action.payload.id) {
+        }
+      }
+      if (index !== -1) {
+        todoTask[index] = action.payload;
+        return todoTask;
+      }
+      break;
+    // we don't use toggleTodo in this project. left for an alternative option
+    case TOGGLE_TODO:
+      todoTask = [...state];
+      todoTask = todoTask.map((todo) => {
+        return todo.id === Number(action.payload.id)
+          ? { ...todo, completed: !todo.completed }
+          : { ...todo };
+      });
+      return todoTask;
+
+    // No Default
+  }
+  return state;
+};
+
+export default reducer;
